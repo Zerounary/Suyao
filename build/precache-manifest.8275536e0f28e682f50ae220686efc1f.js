dd1934f3e9912e79a0dd5e4ea4f2db3acd067feb
Zerounary <850657663@qq.com>
2022-06-18 18:07:08 +0800

    pkg

---
+++ build/precache-manifest.8275536e0f28e682f50ae220686efc1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becc1bbcdfa98316c99efd8ce52530f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc7d37e023c849544263",
+    "url": "./static/css/main.c64da018.chunk.css"
+  },
+  {
+    "revision": "3cf9687c12583b5d273e",
+    "url": "./static/js/2.5c54e7cc.chunk.js"
+  },
+  {
+    "revision": "69df2bda2d75ec8167a98e6172bcf8c5",
+    "url": "./static/js/2.5c54e7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7d37e023c849544263",
+    "url": "./static/js/main.123bfa7a.chunk.js"
+  },
+  {
+    "revision": "794b448f252b9e79d316",
+    "url": "./static/js/runtime-main.c671e08f.js"
+  }
+]);
